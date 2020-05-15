181cb2a587192dc2be9f68be5d9070da7e55f955
YannMU <ymusellec@laposte.net>
2020-05-15 20:22:41 +0200

    Add of the Anemometer and the library TimerOne

---
+++ src/components/sensorActuatorTest/Anemometer/Anemometer.ino
@@ -0,0 +1,89 @@
+#include "TimerOne.h"
+#include <math.h>
+
+#define WindSensor_Pin (2) // digital pin for wind speed sensor
+#define WindVane_Pin (A14) // analog pin for wind direction sensor
+#define VaneOffset 0 // define the offset for caclulating wind direction // TODO, lien avec le nord
+
+volatile bool isSampleRequired; // this is set every 2.5sec to generate wind speed
+volatile unsigned int timerCount; // used to count ticks for 2.5sec timer count
+volatile unsigned long rotations; // cup rotation counter for wind speed calcs
+volatile unsigned long contactBounceTime; // timer to avoid contact bounce in wind speed sensor
+volatile float windSpeed;
+
+int vaneValue; // raw analog value from wind vane
+int vaneDirection; // translated 0 - 360 wind direction
+int calDirection; // calibrated direction after offset applied
+int lastDirValue; // last recorded direction value
+
+void setup() {
+
+  // setup anemometer values
+  lastDirValue = 0;
+  rotations = 0;
+  isSampleRequired = false;
+
+  // setup timer values
+  timerCount = 0;
+  
+  Serial.begin(9600);
+  Serial.println("Davis Sensor Test");
+  Serial.println("tSpeed\tDirection");
+
+  pinMode(WindSensor_Pin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(WindSensor_Pin), isr_rotation, FALLING);
+
+  // setup the timer for 0.5 second
+  Timer1.initialize(500000);
+  Timer1.attachInterrupt(isr_timer);
+
+  sei();// Enable Interrupts
+}
+
+  void loop() {
+  if(isSampleRequired) {
+  
+    getWindDirection();
+
+    Serial.print(windSpeed); Serial.print(" mph\t");
+    Serial.print(calDirection); Serial.println("*");
+    
+    isSampleRequired = false;
+  }
+}
+
+// Interrupt handler routine for timer interrupt
+  void isr_timer() {
+  
+  timerCount++;
+  
+  if(timerCount == 5) {
+    windSpeed = rotations * 0.9;
+    rotations = 0;
+    isSampleRequired = true;
+    timerCount = 0;
+  }
+}
+
+// Interrupt handler routine to increment the rotation count for wind speed
+void isr_rotation() {
+  
+  if((millis() - contactBounceTime) > 15 ) { // debounce the switch contact
+    rotations++;
+    contactBounceTime = millis();
+  }
+}
+
+// Get Wind Direction
+void getWindDirection() {
+
+  vaneValue = analogRead(WindVane_Pin);
+  vaneDirection = map(vaneValue, 0, 1023, 0, 359);
+  calDirection = vaneDirection + VaneOffset;
+  
+  if(calDirection > 360)
+  calDirection = calDirection - 360;
+  
+  if(calDirection > 360)
+  calDirection = calDirection - 360;
+} 
---
+++ src/components/sensorActuatorTest/Anemometer/README.md
@@ -0,0 +1,4 @@
+## The Anemometer and the vane sensor  
+This component is the Davis Anemometer which can give the speed and the orientation of the wind.  
+For this programm, we need to add the library TimerOne.h.  
+You can download this library or find other information here: [link](https://www.arduinolibraries.info/libraries/timer-one "Information for the library TimerOne.h")
